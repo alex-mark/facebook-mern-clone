1036fc3996cf4528d18d8709e004976b8712c884
Alex Markin <alexndr86@gmail.com>
2020-10-13 13:42:01 +0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+
+      {/* App body */}
+      {/* Sidebar */}
+      {/* Feed */}
+      {/* Widgets */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLrI8hgBhMDm_jIwXjXAFI_GN7CJVPXqI",
+  authDomain: "facebook-clone-61815.firebaseapp.com",
+  databaseURL: "https://facebook-clone-61815.firebaseio.com",
+  projectId: "facebook-clone-61815",
+  storageBucket: "facebook-clone-61815.appspot.com",
+  messagingSenderId: "259281911276",
+  appId: "1:259281911276:web:a6ca4d3784f628831014c9",
+  measurementId: "G-6GQFSNZBKS",
+};
