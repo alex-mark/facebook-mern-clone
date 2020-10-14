9d5069388f194a143460a800d91b64fc1cf16f37
Alex Markin <alexndr86@gmail.com>
2020-10-14 17:28:18 +0300

    Style post

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,12 +2,25 @@ import React from "react";
 import "./Feed.css";
 import StoryReel from "./StoryReel";
 import MessageSender from "./MessageSender";
+import Post from "./Post";
 
 function Feed() {
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
+
+      <Post
+        message="Hey, it works"
+        timestamp="the timestamp"
+        username="alexmark_in"
+        image="https://code.org/shared/images/social-media/codeorg2019_social.png"
+      />
+      <Post
+        message="Hey, it works"
+        timestamp="the timestamp"
+        username="alexmark_in"
+      />
     </div>
   );
 }
