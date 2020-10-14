978d8ec6cb9442b65d75b3a28fed59ea17e1e5ce
Alex Markin <alexndr86@gmail.com>
2020-10-14 21:57:42 +0300

    Add firebase init

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyDLrI8hgBhMDm_jIwXjXAFI_GN7CJVPXqI",
   authDomain: "facebook-clone-61815.firebaseapp.com",
@@ -9,3 +10,11 @@ const firebaseConfig = {
   appId: "1:259281911276:web:a6ca4d3784f628831014c9",
   measurementId: "G-6GQFSNZBKS",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
