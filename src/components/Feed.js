64280e168e63200f4bf7314be35fa32ec5de9928
Alex Markin <alexndr86@gmail.com>
2020-10-15 10:55:19 +0300

    Add db connection

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Feed.css";
 import StoryReel from "./StoryReel";
 import MessageSender from "./MessageSender";
 import Post from "./Post";
+import db from "../firebase";
 
 function Feed() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    db.collection("posts").onSnapshot((snapshot) =>
+      setPosts(snapshot.docs.map((doc) => ({ id: doc.id, data: doc.data() })))
+    );
+  }, []);
+
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
 
-      <Post
-        message="Hey, it works"
-        timestamp="the timestamp"
-        username="alexmark_in"
-        image="https://code.org/shared/images/social-media/codeorg2019_social.png"
-      />
-      <Post
-        message="Hey, it works"
-        timestamp="the timestamp"
-        username="alexmark_in"
-      />
+      {posts.map((post) => (
+        <Post
+          key={post.id}
+          message={post.message}
+          timestamp={post.timestamp}
+          username={post.username}
+          image={post.image}
+        />
+      ))}
     </div>
   );
 }
