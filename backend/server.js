11d2587414001a363382690a131ea65f90fc911e
Alex Markin <alexndr86@gmail.com>
2020-10-19 14:27:25 +0300

    Setup basic express server

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import multer from "multer";
+import GridFsStorage from "multer-gridfs-storage";
+import Grid from "gridfs-stream";
+import bodyParser from "body-parser";
+import path from "path";
+import Pusher from "pusher";
+
+Grid.mongo = mongoose.mongo;
+
 // app config
+const app = express();
+const port = process.env.PORT || 9000;
 
 // middlewares
+app.use(bodyParser.json());
+app.use(cors());
 
 // db config
 
 // api routes
+app.get("/", (req, res) => res.status(200).send("Hello world!"));
 
 // listen
