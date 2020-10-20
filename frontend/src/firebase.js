885a67e83e63e1638979869b26b1e89185ac53a0
Alex Markin <alexndr86@gmail.com>
2020-10-20 18:07:57 +0300

    Change firebase credentials

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLrI8hgBhMDm_jIwXjXAFI_GN7CJVPXqI",
-  authDomain: "facebook-clone-61815.firebaseapp.com",
-  databaseURL: "https://facebook-clone-61815.firebaseio.com",
-  projectId: "facebook-clone-61815",
-  storageBucket: "facebook-clone-61815.appspot.com",
-  messagingSenderId: "259281911276",
-  appId: "1:259281911276:web:a6ca4d3784f628831014c9",
-  measurementId: "G-6GQFSNZBKS",
+  apiKey: "AIzaSyB1yMMOrCaCVtSN5rhwdzSch2O7_c12eBY",
+  authDomain: "facebook-mern-clone-22737.firebaseapp.com",
+  databaseURL: "https://facebook-mern-clone-22737.firebaseio.com",
+  projectId: "facebook-mern-clone-22737",
+  storageBucket: "facebook-mern-clone-22737.appspot.com",
+  messagingSenderId: "453417083421",
+  appId: "1:453417083421:web:4f1390443225004cf25cf4",
+  measurementId: "G-VGZET078M2",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
